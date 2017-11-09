d5571cc0aa16eb77266400c93db7a8218350d004
BurtonFeng <bfeng@albany.edu>
2017-11-09 18:06:52 -0500

    Bot replies to hello

--- src/main/java/Discord/Bot/App.java
+++ src/main/java/Discord/Bot/App.java
@@ -1,13 +1,33 @@
 package Discord.Bot;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class App extends ListenerAdapter {
+	private static JDA jda;
+	private final static String token = "Mzc4MDMyMzQ0MTMwMzg3OTcw.DOZwng.QM-Jzse-fLQHU1DEvwkUFt4gFKs";
+    public static void main(String[] args) throws LoginException, IllegalArgumentException, InterruptedException, RateLimitedException {
+    	jda = new JDABuilder(AccountType.BOT).setToken(token).buildBlocking();
+    	jda.addEventListener(new App());
+    	
+    }
+    
+    @Override
+    public void onMessageReceived(MessageReceivedEvent e) {
+    	Message msg = e.getMessage();
+    	MessageChannel chn = e.getChannel();
+    	User usr = e.getAuthor();
+    	
+    	if(msg.getContent().equals("hello"))
+    		chn.sendMessage("Hello, " + usr.getAsMention() + "!").queue();
     }
 }
